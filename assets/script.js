c29ce00c23a8f456fb22550a600529771cd19dd4
Kirsten <kh_2015@yahoo.com>
2023-02-24 13:16:14 +0000

    local storage and ReadMe

--- assets/script.js
+++ assets/script.js
@@ -5,17 +5,16 @@
   //Put the current date into the Jumbotron
   let displayDate = document.getElementById("currentDay");
   displayDate.innerHTML = Today;
-  
-});
+    //get the present hour
+  let thisHour = moment().format("HH");
+
 
   //Compare the time block on grid to actual hour and change class to so colour styling is applied
   $(".time-div").each(function () {
     var timeBlock = $(this).attr("id").split("-")[1];
-     //get the present hour
-    let thisHour = moment().format("HH");
+   
     if (thisHour == timeBlock) {
       $(this).addClass("present");
-      $(this).children(".description").addClass("white-text");
     } else if (thisHour < timeBlock) {
       $(this).removeClass("present");
       $(this).addClass("future");
@@ -24,3 +23,24 @@
       $(this).addClass("past");
     }
   });
+
+    //Activating the time block 'save button' clicks to store the data from the input in local storage
+    $(".saveBtn").click(function (event) {
+      event.preventDefault();
+      var value = $(this).siblings(".time-block").val();
+      var time = $(this).parent().attr("id").split("-")[1];
+      localStorage.setItem(time, value);
+    });
+
+    //Retrieving data from local storage 
+      //retrieves items from local storage and sets them in proper places
+  $("#hr-09 .time-block").val(localStorage.getItem("09"));
+  $("#hr-10 .time-block").val(localStorage.getItem("10"));
+  $("#hr-11 .time-block").val(localStorage.getItem("11"));
+  $("#hr-12 .time-block").val(localStorage.getItem("12"));
+  $("#hr-13 .time-block").val(localStorage.getItem("13"));
+  $("#hr-14 .time-block").val(localStorage.getItem("14"));
+  $("#hr-15 .time-block").val(localStorage.getItem("15"));
+  $("#hr-16 .time-block").val(localStorage.getItem("16"));
+  $("#hr-17 .time-block").val(localStorage.getItem("17"));
+});
